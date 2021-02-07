2f0c6c7729ffe5db35468e1146c392a8a33d76e4
Muhammet <33325253+blackdragonx61@users.noreply.github.com>
2021-02-07 10:32:17 +0300

    Update input_main.cpp

--- 1.Svn/Server/game/src/input_main.cpp
+++ 1.Svn/Server/game/src/input_main.cpp
@@ -6,15 +6,32 @@ void CInputMain::SafeboxItemMove(LPCHARACTER ch, const char* data)
 
 ///Add
 #if defined(BL_REMOTE_SHOP)
+static bool RemoteShopRestrictedMap(long lMapIndex)
+{
+	switch (lMapIndex)
+	{
+	case 113:
+		return false;
+	}
+	return true;
+}
 void CInputMain::RemoteShop(LPCHARACTER ch, const char* data)
 {
 	const TPacketCGRemoteShop* p = reinterpret_cast<const TPacketCGRemoteShop*>(data);
-	static const/*expr*/ DWORD _arrShopNpcVnum[] = { 9001, 9002, 9003, 9004 };
 
-	if (p->shop_index >= _countof(_arrShopNpcVnum)) {
-		sys_err("RemoteShop player(%s) unknown index(%d)", ch->GetName(), p->shop_index);
+	if (RemoteShopRestrictedMap(ch->GetMapIndex()) == false)
+	{
+		ch->ChatPacket(CHAT_TYPE_INFO, "<RemoteShop> You can not open in this map.");
 		return;
 	}
+
+	//PREVENT_TRADE_WINDOW
+	if (ch->IsOpenSafebox() || ch->GetExchange() || ch->GetShopOwner() || ch->GetMyShop() || ch->IsCubeOpen())
+	{
+		ch->ChatPacket(CHAT_TYPE_INFO, LC_TEXT("ґЩёҐ °Е·ЎГўАМ ї­ё°»уЕВїЎј­ґВ »уБЎ°Е·Ўё¦ ЗТјц °Ў ѕшЅАґПґЩ."));
+		return;
+	}
+	//END_PREVENT_TRADE_WINDOW
 
 	const DWORD dwCurrentTime = get_dword_time(), dwLimitTime = ch->GetLastRemoteTime() + 15000; // 15 sec.
 	if (dwCurrentTime < dwLimitTime) {
@@ -22,36 +39,21 @@ void CInputMain::RemoteShop(LPCHARACTER ch, const char* data)
 		return;
 	}
 
-	LPSECTREE_MAP pSecMap = SECTREE_MANAGER::instance().GetMap(ch->GetMapIndex());
-	if (!pSecMap)
-		return;
-
-	struct FindSHOP
-	{
-		LPCHARACTER npc;
-		DWORD vnum;
-		FindSHOP(DWORD m_vnum) : npc(NULL), vnum(m_vnum) {};
-		void operator()(LPENTITY ent)
-		{
-			if (ent->IsType(ENTITY_CHARACTER)) {
-				LPCHARACTER ch = static_cast<LPCHARACTER>(ent);
-				if (ch && ch->IsNPC() && ch->GetRaceNum() == vnum)
-					npc = ch;
-			}
-		}
-	};
-
-	FindSHOP func(_arrShopNpcVnum[p->shop_index]);
-	pSecMap->for_each(func);
-
-	LPCHARACTER ShopNPC = func.npc;
-	if (ShopNPC == NULL) {
-		ch->ChatPacket(CHAT_TYPE_INFO, "This shop does not exist on this map.");
+	static const/*expr*/ DWORD _arrShopVnum[] = { 70, 75, 48, 107 };
+	if (p->shop_index >= _countof(_arrShopVnum)) {
+		sys_err("RemoteShop player(%s) unknown index(%d)", ch->GetName(), p->shop_index);
 		return;
 	}
 
-	CShopManager::instance().StartShopping(ch, ShopNPC);
+	const DWORD dwShopVnum = _arrShopVnum[p->shop_index];
+	LPSHOP pkShop = CShopManager::instance().Get(dwShopVnum);
+	if (pkShop == NULL)
+		return;
+
+	pkShop->AddGuest(ch, NULL, false);
+	ch->SetShopOwner(ch);
 	ch->SetLastRemoteTime(get_dword_time());
+	sys_log(0, "SHOP: START: %s", ch->GetName());
 }
 #endif
 
